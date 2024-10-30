e7b2daaf57386617a736e0ef16fb19ac0c11ecef
andrewjbe <56839927+andrewjbe@users.noreply.github.com>
2024-10-30 12:08:35 -0500

    fixed default geom colors in ojo_set_theme

--- R/ojo_set_theme.R
+++ R/ojo_set_theme.R
@@ -25,45 +25,36 @@ okpi_set_theme <- function(style = "print",
   # set default theme to theme_ojo_*() --------------------------------------
 
   if (style == "print") {
-
     ggplot2::theme_set(theme_ojo_print(base_size = base_size,
                                        base_family = base_family,
                                        base_line_size = base_line_size,
                                        base_rect_size = base_rect_size))
-
   } else if (style == "map") {
-
     ggplot2::theme_set(theme_ojo_map(base_size = base_size,
                                      base_family = base_family,
                                      base_line_size = base_line_size,
                                      base_rect_size = base_rect_size,
                                      scale = scale))
-
   } else {
-
     stop('Invalid "style" argument. Valid styles are: ',
          '"print" and "map".',
          call. = FALSE
     )
-
   }
 
   # add base_family font to text and label geoms ---------------------------
-
   ggplot2::update_geom_defaults("text", list(family = base_family))
   ggplot2::update_geom_defaults("label", list(family = base_family))
   ggplot2::update_geom_defaults("text_repel", list(family = base_family))
   ggplot2::update_geom_defaults("label_repel", list(family = base_family))
 
   # set default color scales for continuous variables -----------------------
-
   options(
     ggplot2.continuous.colour = "gradient",
     ggplot2.continuous.fill = "gradient"
   )
 
   # set default colors for monochromatic geoms ------------------------------
-
   ggplot2::update_geom_defaults("bar", list(fill = ojothemes::okpi_blue))
   ggplot2::update_geom_defaults("col", list(fill = ojothemes::okpi_blue))
   ggplot2::update_geom_defaults("point", list(colour = ojothemes::okpi_blue))
@@ -75,7 +66,6 @@ okpi_set_theme <- function(style = "print",
   ggplot2::update_geom_defaults("violin", list(fill = ojothemes::okpi_blue))
 
   # set default colors for monochromatic stats ------------------------------
-
   ggplot2::update_stat_defaults("count", list(fill = ojothemes::okpi_blue))
   ggplot2::update_stat_defaults("boxplot", list(fill = ojothemes::okpi_blue))
   ggplot2::update_stat_defaults("density", list(fill = ojothemes::okpi_blue))
