358caef7cd8322f4e31ec7ea4b3dfee9836abd6b
andrewjbe <56839927+andrewjbe@users.noreply.github.com>
2024-11-01 14:33:53 -0500

    added gt_ojo() and gt_okpi() functions

--- R/ojo_gt.R
+++ R/ojo_gt.R
@@ -1,18 +1,62 @@
-#' ojo_gt Function
+#' gt_okpi Function
 #'
 #' @param data A data frame to be converted into a gt table.
 #' @param title Optional. A title for the table. Default is NA.
 #' @param subtitle Optional. A subtitle for the table. Default is NA.
+#' @param font_size The font size for the table. Default is 14.
+#' @param font The font family to use. Default is Roboto Condensed.
+#' @param format_cols Should gt::fmt_auto() be applied to all cols?
+#' @param analyst_name The name of the analyst to credit in the footnote
+#' @param source The source / domain of the data
+#'
 #' @return A gt table based on the input data frame with specified modifications.
 #' @examples
 #' \dontrun{
-#' ojo_gt(data = mtcars, title = "Motor Trend Car Road Tests", subtitle = "From mtcars")
+#' okpi_gt(data = mtcars, title = "Motor Trend Car Road Tests", subtitle = "From mtcars")
 #' }
 #' @export
-ojo_gt <- function(data, title = NA, subtitle = NA) {
+gt_okpi <- function(data,
+                   title = NA, subtitle = NA,
+                   font_size = 14,
+                   font = "Roboto Condensed",
+                   format_cols = TRUE,
+                   analyst_name = NA, source = NA
+                   ) {
+
   x <- data |>
     gt::gt() |>
-    gtExtras::gt_theme_nytimes() |>
+    gt::tab_options(
+      heading.align = "left",
+      column_labels.border.top.style = "none",
+      table.border.top.style = "none",
+      column_labels.border.bottom.style = "none",
+      column_labels.border.bottom.width = 1,
+      column_labels.border.bottom.color = "#A9A9A9",
+      table_body.border.top.style = "none",
+      table_body.border.bottom.color = "white",
+      heading.border.bottom.style = "none",
+      data_row.padding = gt::px(7),
+      column_labels.font.size = gt::px(font_size)
+    ) |>
+    gt::opt_table_font(
+      font = gt::google_font(font)
+    ) |>
+    gt::tab_style(
+      style = gt::cell_text(weight = "bold",
+                            color = ojothemes::okpi_red,
+                            size = gt::px(font_size * 2)),
+      locations = gt::cells_title(groups = "title")
+    ) |>
+    gt::tab_style(
+      style = gt::cell_text(style = "italic",
+                            size = gt::px(font_size * 1.25)),
+      locations = gt::cells_title(groups = "subtitle")
+    ) |>
+    gt::tab_style(
+      style = gt::cell_text(color = "#A9A9A9",
+                            transform = "uppercase"),
+      locations = gt::cells_column_labels(tidyselect::everything())
+    ) |>
     gt::tab_spanner(
       label = stringr::str_to_title(colnames(data))
     ) |>
@@ -30,5 +74,109 @@ ojo_gt <- function(data, title = NA, subtitle = NA) {
                      subtitle = subtitle)
   }
 
+  # Add col formatting?
+  if (format_cols) {
+    x <- x |>
+      gt::fmt_auto(lg_num_pref = "suf")
+  }
+
+  if (!is.na(analyst_name) | !is.na(source)) {
+    x <- x |>
+      ojo_gt_captions(analyst_name = analyst_name,
+                      source = source)
+  }
+
   return(x)
 }
+
+#' gt_ojo Function
+#'
+#' @param data A data frame to be converted into a gt table.
+#' @param title Optional. A title for the table. Default is NA.
+#' @param subtitle Optional. A subtitle for the table. Default is NA.
+#' @param font_size The font size for the table. Default is 14.
+#' @param font The font family to use. Default is Roboto Condensed.
+#' @param format_cols Should gt::fmt_auto() be applied to all cols?
+#' @param analyst_name The name of the analyst to credit in the footnote
+#' @param source The source / domain of the data
+#'
+#' @return A gt table based on the input data frame with specified modifications.
+#' @examples
+#' \dontrun{
+#' okpi_gt(data = mtcars, title = "Motor Trend Car Road Tests", subtitle = "From mtcars")
+#' }
+#' @export
+gt_ojo <- function(data,
+                   title = NA, subtitle = NA,
+                   font_size = 14,
+                   font = "Roboto Mono",
+                   format_cols = TRUE,
+                   analyst_name = NA, source = NA
+) {
+
+  x <- data |>
+    gt::gt() |>
+    gt::tab_options(
+      heading.align = "left",
+      column_labels.border.top.style = "none",
+      table.border.top.style = "none",
+      column_labels.border.bottom.style = "none",
+      column_labels.border.bottom.width = 1,
+      column_labels.border.bottom.color = "#A9A9A9",
+      table_body.border.top.style = "none",
+      table_body.border.bottom.color = "white",
+      heading.border.bottom.style = "none",
+      data_row.padding = gt::px(7),
+      column_labels.font.size = gt::px(font_size)
+    ) |>
+    gt::opt_table_font(
+      font = gt::google_font(font)
+    ) |>
+    gt::tab_style(
+      style = gt::cell_text(weight = "bold",
+                            color = "#333333",
+                            size = gt::px(font_size * 2)),
+      locations = gt::cells_title(groups = "title")
+    ) |>
+    gt::tab_style(
+      style = gt::cell_text(style = "italic",
+                            size = gt::px(font_size * 1.25)),
+      locations = gt::cells_title(groups = "subtitle")
+    ) |>
+    gt::tab_style(
+      style = gt::cell_text(color = "#A9A9A9",
+                            transform = "uppercase"),
+      locations = gt::cells_column_labels(tidyselect::everything())
+    ) |>
+    gt::tab_spanner(
+      label = stringr::str_to_title(colnames(data))
+    ) |>
+    gt::tab_options(
+      column_labels.font.size = "medium",
+      table.font.size = "medium",
+      heading.title.font.size = "medium",
+      heading.subtitle.font.size = "small"
+    )
+
+  # Add title / subtitle?
+  if (!is.na(title) | !is.na(subtitle)) {
+    x <- x |>
+      gt::tab_header(title = title,
+                     subtitle = subtitle)
+  }
+
+  # Add col formatting?
+  if (format_cols) {
+    x <- x |>
+      gt::fmt_auto(lg_num_pref = "suf")
+  }
+
+  if (!is.na(analyst_name) | !is.na(source)) {
+    x <- x |>
+      ojo_gt_captions(analyst_name = analyst_name,
+                      source = source)
+  }
+
+  return(x)
+}
+
--- R/ojo_labs.R
+++ R/ojo_labs.R
@@ -1,14 +1,11 @@
-#' OJO caption text
+#' OJO source text
 #' @description
-#' Creates the text for a caption to add to ggplots and gt tables, including consistent default "source: " statements.
+#' A function to construct the "Source:" note text
 #'
 #' @param source The domain / source of the data. Can be one of "oscn", "ocdc", or "ppb" for canned text, NA for no source, or a custom string.
-#' @param analyst_name The name of the analyst to credit
-#' @returns A string with the caption text
+#' @returns A string with the source text
 #' @export
-ojo_make_caption <- function(analyst_name = NA,
-                             source = NA){
-
+ojo_source_text <- function(source = NA) {
   # Check that given source is in the list of options # Removed to allow for custom string
   # allowed_sources <- c("oscn", "ocdc", "ppb", NA)
   # source <- tolower(source)
@@ -22,13 +19,39 @@ ojo_make_caption <- function(analyst_name = NA,
     is.na(source) ~ "", # If source = NA, just leave that out of the caption
     TRUE ~ source # If source is anything else, just print it verbatim
   )
+  return(source_text)
+}
 
+#' OJO analyst name text
+#' @description
+#' A function to construct the analyst credit text
+#'
+#' @param analyst_name The name of the analyst to credit
+#' @returns A string with the analyst credit text
+#' @export
+ojo_analyst_name_text <- function(analyst_name = NA) {
   # Construct the caption text (could build in analyst emails here too? i.e. "Chart created by Andrew Bell (abell@okpolicy.org)")
   analyst_name_text <- if(!is.na(analyst_name)){
-    paste0("Chart created by ", analyst_name, ".")
+    paste0("Graphic created by ", analyst_name, ".")
   } else {
     ""
   }
+  return(analyst_name_text)
+}
+
+#' OJO caption text
+#' @description
+#' Creates the text for a caption to add to ggplots and gt tables, including consistent default "source: " statements.
+#'
+#' @param source The domain / source of the data. Can be one of "oscn", "ocdc", or "ppb" for canned text, NA for no source, or a custom string.
+#' @param analyst_name The name of the analyst to credit
+#' @returns A string with the caption text
+#' @export
+ojo_make_caption <- function(analyst_name = NA,
+                             source = NA){
+
+  source_text <- ojo_source_text(source = source)
+  analyst_name_text <- ojo_analyst_name_text(analyst_name = analyst_name)
 
   caption <- paste0(source_text,
                     dplyr::if_else(!is.na(analyst_name) & !is.na(source), "\n", ""), # Add a newline if both are not NA
@@ -59,4 +82,35 @@ ojo_labs <- function (...,
     caption = ojo_make_caption(analyst_name = analyst_name, source = source)
   )
 
+}
+
+#' OJO gt caption / source note with defaults
+#' @description
+#' Wrapper for gt::tab_source_note() and gt::tab_footnote() with consistent defaults.
+#'
+#' @param x A gt object
+#' @param analyst_name The name of the analyst to credit
+#' @param source The data source / source of the data.
+#'
+#' @export
+ojo_gt_captions <- function (x,
+                             analyst_name = NA,
+                             source = NA){
+
+  # Do we want to build any more QOL stuff into this? Like we could set it up
+  # so that it automatically decides when to display an axis label, formats
+  # those to be title case, etc.
+  x <- x |>
+    gt::tab_source_note(
+      source_note = ojo_source_text(source = source)
+    ) |>
+    gt::tab_footnote(
+      footnote = ojo_analyst_name_text(analyst_name = analyst_name)
+    ) |>
+    gt::tab_style(
+      style = gt::cell_text(align = "right"),
+      locations = list(gt::cells_source_notes(), gt::cells_footnotes())
+    )
+
+  return(x)
 }
