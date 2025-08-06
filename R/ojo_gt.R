b50aaed5e8eae0c88f19a44e98366ce70a3b3c62
andrewjbe <56839927+andrewjbe@users.noreply.github.com>
2025-08-06 16:50:08 -0500

    removing bug that prevented spanner lines from showing up, and attempting font fix

--- R/ojo_gt.R
+++ R/ojo_gt.R
@@ -28,17 +28,11 @@ gt_base <- function(data,
       heading.align = "left",
       column_labels.border.top.style = "none",
       table.border.top.style = "none",
-      column_labels.border.bottom.style = "none",
-      column_labels.border.bottom.width = 1,
-      column_labels.border.bottom.color = "#A9A9A9",
-      table_body.border.top.style = "none",
-      table_body.border.bottom.color = "white",
+      column_labels.border.bottom.width = 3,
       heading.border.bottom.style = "none",
       data_row.padding = gt::px(7),
-      column_labels.font.size = gt::px(font_size)
-    ) |>
-    gt::tab_spanner(
-      label = stringr::str_to_title(colnames(data))
+      column_labels.font.size = gt::px(font_size),
+      table.border.bottom.style = "none"
     ) |>
     gt::tab_options(
       column_labels.font.size = "medium",
@@ -46,12 +40,14 @@ gt_base <- function(data,
       heading.title.font.size = "medium",
       heading.subtitle.font.size = "small"
     ) |>
+    # Bold / uppercase column names
     gt::tab_style(
       style = gt::cell_text(color = "#333333",
                             transform = "uppercase",
                             weight = "bold"),
       locations = gt::cells_column_labels(tidyselect::everything())
     ) |>
+    # Bold / big title
     gt::tab_style(
       style = list(
         gt::cell_text(weight = "bold",
@@ -59,6 +55,7 @@ gt_base <- function(data,
       ),
       locations = gt::cells_title(groups = "title")
     ) |>
+    # Italicized / slightly big subtitle
     gt::tab_style(
       style = list(
         gt::cell_text(style = "italic",
@@ -66,6 +63,12 @@ gt_base <- function(data,
                       size = gt::px(font_size * 1.25))
       ),
       locations = gt::cells_title(groups = "subtitle")
+    ) |>
+    gt::tab_style(
+      style = gt::cell_text(color = "#333333",
+                            transform = "uppercase",
+                            weight = "bold"),
+      locations = gt::cells_column_spanners(tidyselect::everything())
     )
 
 
@@ -97,7 +100,7 @@ gt_base <- function(data,
 #' @param title Optional. A title for the table. Default is NA.
 #' @param subtitle Optional. A subtitle for the table. Default is NA.
 #' @param font_size The font size for the table. Default is 14.
-#' @param font The font family to use. Default is Roboto Condensed.
+#' @param chosen_font The font family to use. Default is Roboto Condensed.
 #' @param format_cols Should gt::fmt_auto() be applied to all cols?
 #' @param analyst_name The name of the analyst to credit in the footnote
 #' @param source The source / domain of the data
@@ -111,7 +114,7 @@ gt_base <- function(data,
 gt_okpi <- function(data,
                     title = NA, subtitle = NA,
                     font_size = 14,
-                    font = "Roboto Condensed",
+                    chosen_font = "Roboto Condensed",
                     format_cols = TRUE,
                     analyst_name = NA, source = NA){
 
@@ -123,8 +126,9 @@ gt_okpi <- function(data,
                        analyst_name = analyst_name,
                        source = source) |>
     gt::opt_table_font(
-      font = gt::google_font(font)
+      font = chosen_font
     ) |>
+    # Format title
     gt::tab_style(
       style = list(
         gt::cell_text(color = ojothemes::okpi_red),
@@ -133,6 +137,7 @@ gt_okpi <- function(data,
       ),
       locations = gt::cells_title(groups = "title")
     ) |>
+    # Format subtitle
     gt::tab_style(
       style = list(
         gt::css(padding.left = "10px",
@@ -140,6 +145,17 @@ gt_okpi <- function(data,
         ),
       locations = gt::cells_title(groups = "subtitle")
     )
+    # # Format spanner headers
+    # gt::tab_style(
+    #   style = list(
+    #     gt::cell_text(
+    #       font = chosen_font,
+    #       weight = "bold",
+    #       size = "larger"
+    #     )
+    #   ),
+    #   locations = gt::cells_column_spanners()
+    # )
 
   return(x)
 }
@@ -150,7 +166,7 @@ gt_okpi <- function(data,
 #' @param title Optional. A title for the table. Default is NA.
 #' @param subtitle Optional. A subtitle for the table. Default is NA.
 #' @param font_size The font size for the table. Default is 14.
-#' @param font The font family to use. Default is Roboto mono
+#' @param chosen_font The font family to use. Default is Roboto mono
 #' @param format_cols Should gt::fmt_auto() be applied to all cols?
 #' @param analyst_name The name of the analyst to credit in the footnote
 #' @param source The source / domain of the data
@@ -164,7 +180,7 @@ gt_okpi <- function(data,
 gt_ojo <- function(data,
                    title = NA, subtitle = NA,
                    font_size = 14,
-                   font = "Roboto Mono",
+                   chosen_font = "Roboto Mono",
                    format_cols = TRUE,
                    analyst_name = NA, source = NA
 ) {
@@ -177,7 +193,7 @@ gt_ojo <- function(data,
                        analyst_name = analyst_name,
                        source = source) |>
     gt::opt_table_font(
-      font = gt::google_font(font)
+      font = chosen_font
     ) |>
     gt::tab_style(
       style = list(
